65d51d6b8fd488576221f416b2bc53bb3294711a
Tuğrulhan <101876837+tugrul07@users.noreply.github.com>
2023-05-28 14:45:46 +0300

    Add files via upload

---
+++ musteri.js
@@ -0,0 +1,10 @@
+// BURDA OLUŞTULAN MÜŞTERİ CLASSI MİGROSBASE CLASSINI İNHERİT EDECEK
+class Musteri extends MigrosBase{
+    constructor(isim,soyisim,urunler,kartVarmi){
+        super(isim,soyisim,urunler,kartVarmi)
+    }
+    hesapla(){
+         
+        return super.hesapla();
+    }
+}   
